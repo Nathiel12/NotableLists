ab3aaaa5cadefd5b14a161086a58aa2ad5d3067f
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-11-14 15:24:18 -0600

    Cambiado userId a id y el tipo a string, tambien agregar nuevo metodo

--- app/src/main/java/ucne/edu/notablelists/domain/users/repository/UserRepository.kt
+++ app/src/main/java/ucne/edu/notablelists/domain/users/repository/UserRepository.kt
@@ -1,11 +1,14 @@
 package ucne.edu.notablelists.domain.users.repository
 
+import ucne.edu.notablelists.data.remote.Resource
 import ucne.edu.notablelists.domain.users.model.User
 
 interface UserRepository {
-    suspend fun getUser(userId: Int): User?
+    suspend fun getUser(id: String): User?
+    suspend fun createUserLocal(user: User): Resource<User>
     suspend fun postUser(user: User): User
     suspend fun putUser(user: User): User
-    suspend fun deleteUser(userId: Int)
-    suspend fun upsertUser(user: User):Int
+    suspend fun deleteUser(id: String): Resource<Unit>
+    suspend fun upsertUser(user: User):Resource<Unit>
+    suspend fun postPendingUsers(): Resource<Unit>
 }
