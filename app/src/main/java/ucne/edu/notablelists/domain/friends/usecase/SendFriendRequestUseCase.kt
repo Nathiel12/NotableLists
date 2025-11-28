28c6f74b14f5016631377a57d1512d296906735b
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-11-28 16:34:24 -0600

    Creado SendFriendRequestUseCase.kt

---
+++ app/src/main/java/ucne/edu/notablelists/domain/friends/usecase/SendFriendRequestUseCase.kt
@@ -0,0 +1,11 @@
+package ucne.edu.notablelists.domain.friends.usecase
+
+import ucne.edu.notablelists.data.remote.Resource
+import ucne.edu.notablelists.domain.friends.FriendsRepository
+import javax.inject.Inject
+
+class SendFriendRequestUseCase @Inject constructor(
+    private val repository: FriendsRepository
+) {
+    suspend operator fun invoke(userId: Int, friendId: Int): Resource<Unit> = repository.sendFriendRequest(userId, friendId)
+}
