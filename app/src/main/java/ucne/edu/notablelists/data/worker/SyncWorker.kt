ccac67171b3411427b4751114593fffd8f99d038
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-11-30 11:53:21 -0600

    Now passes userId correctly

--- app/src/main/java/ucne/edu/notablelists/data/worker/SyncWorker.kt
+++ app/src/main/java/ucne/edu/notablelists/data/worker/SyncWorker.kt
@@ -1,11 +1,13 @@
 package ucne.edu.notablelists.data.worker
 
+import ucne.edu.notablelists.domain.session.usecase.GetUserIdUseCase
 import android.content.Context
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.first
 import ucne.edu.notablelists.data.remote.Resource
 import ucne.edu.notablelists.domain.notes.repository.NoteRepository
 
@@ -14,14 +16,21 @@ import ucne.edu.notablelists.domain.notes.repository.NoteRepository
 class SyncWorker @AssistedInject constructor(
     @Assisted context: Context,
     @Assisted workerParams: WorkerParameters,
-    private val noteRepository: NoteRepository
+    private val noteRepository: NoteRepository,
+    private val getUserIdUseCase: GetUserIdUseCase
 ) : CoroutineWorker(context, workerParams) {
 
     override suspend fun doWork(): Result {
-        return when (noteRepository.postPendingNotes()) {
-            is Resource.Success -> Result.success()
-            is Resource.Error -> Result.retry()
-            else -> Result.failure()
+        val userId = getUserIdUseCase().first()
+
+        return if (userId != null) {
+            when (noteRepository.postPendingNotes(userId)) {
+                is Resource.Success -> Result.success()
+                is Resource.Error -> Result.retry()
+                else -> Result.failure()
+            }
+        } else {
+            Result.success()
         }
     }
 }
