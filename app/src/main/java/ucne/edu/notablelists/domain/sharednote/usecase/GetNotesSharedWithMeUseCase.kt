30adce3a452b28e27415f2618b63bb6708e755aa
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-12-01 23:16:05 -0600

    refactor: removes mapper from use case

--- app/src/main/java/ucne/edu/notablelists/domain/sharednote/usecase/GetNotesSharedWithMeUseCase.kt
+++ app/src/main/java/ucne/edu/notablelists/domain/sharednote/usecase/GetNotesSharedWithMeUseCase.kt
@@ -1,5 +1,6 @@
 package ucne.edu.notablelists.domain.sharednote.usecase
 
+import ucne.edu.notablelists.data.mappers.toDomainNote
 import ucne.edu.notablelists.data.remote.Resource
 import ucne.edu.notablelists.data.remote.dto.SharedNoteWithDetailsDto
 import ucne.edu.notablelists.domain.notes.model.Note
@@ -12,26 +13,11 @@ class GetNotesSharedWithMeUseCase @Inject constructor(
     suspend operator fun invoke(userId: Int): Resource<List<Note>> {
         return when (val result = repository.getNotesSharedWithMe(userId)) {
             is Resource.Success -> {
-                val notes = result.data?.map { it.toDomain() } ?: emptyList()
+                val notes = result.data?.map { it.toDomainNote() } ?: emptyList()
                 Resource.Success(notes)
             }
             is Resource.Error -> Resource.Error(result.message)
             is Resource.Loading -> Resource.Loading()
         }
-    }
-
-    private fun SharedNoteWithDetailsDto.toDomain(): Note {
-        return Note(
-            id = this.noteId.toString(),
-            remoteId = this.noteId,
-            userId = this.ownerUserId,
-            title = this.noteTitle,
-            description = this.toString(), //esto está malo y no, este comentario no es de IA
-            tag = this.tag ?: "",
-            priority = this.priority,
-            isFinished = this.isFinished,
-            reminder = null,
-            checklist = null
-        )
     }
 }
